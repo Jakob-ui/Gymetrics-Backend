aa7bf47fefcac040e8c3abc6751f8b2ac921ee73
Jakob-ui <Jakoblaschober@gmail.com>
2026-01-10 17:53:33 +0100

    added cors and better error messages

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @HttpCode(HttpStatus.OK)
+  ping(): { status: string; timestamp: Date } {
+    return {
+      status: 'ok',
+      timestamp: new Date(),
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); //Nachher wieder löschen bzw umkonfigurieren!!!!
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
