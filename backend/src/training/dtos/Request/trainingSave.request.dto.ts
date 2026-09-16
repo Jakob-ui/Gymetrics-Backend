0dd87f10d28bbd997f447520ff5c6da21f5cf5a7
Jakob Laschober <64865305+Jakob-ui@users.noreply.github.com>
2026-09-17 00:24:52 +0200

    Release hotfix (#16)
    * added better handling for saving a training
    
    * added some final fixxes and activate cors
    
    * fixing linting error
    
    * added class validators

--- backend/src/training/dtos/Request/trainingSave.request.dto.ts
+++ backend/src/training/dtos/Request/trainingSave.request.dto.ts
@@ -1,16 +1,46 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsBoolean,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
 export class ExerciseDoneRequestDto {
+  @IsOptional()
+  @IsString()
   _id?: string;
-  title: string;
+
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsOptional()
+  @IsNumber()
   repsDone?: number;
+
+  @IsOptional()
+  @IsNumber()
   weightDone?: number;
+
   constructor(init?: Partial<ExerciseDoneRequestDto>) {
     Object.assign(this, init);
   }
 }
 
 export class TrainingDoneRequestDto {
-  active: boolean;
-  plan: ExerciseDoneRequestDto[];
+  @IsOptional()
+  @IsBoolean()
+  active?: boolean;
+
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ExerciseDoneRequestDto)
+  plan?: ExerciseDoneRequestDto[];
+
   constructor(init?: Partial<TrainingDoneRequestDto>) {
     Object.assign(this, init);
   }
